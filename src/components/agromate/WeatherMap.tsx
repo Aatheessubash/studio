ba4ae9bd0beabf07c9cd7ce1222589da36b7bbfd
Aathees <aatheessubash48@gmail.com>
2025-08-08 02:20:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/agromate/WeatherMap.tsx
+++ src/components/agromate/WeatherMap.tsx
@@ -37,7 +37,7 @@ const WeatherMap: FC<WeatherMapProps> = ({ lat, lon }) => {
 
   return (
     <div className="h-64 w-full rounded-lg overflow-hidden border">
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
+        <MapContainer key={`${lat}-${lon}`} center={position} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
